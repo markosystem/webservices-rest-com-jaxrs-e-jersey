fe07d7e16d125738dcf869b32799385387df8659
Marcos Batista <markos.system@gmail.com>
2019-10-13 01:26:03 -0300

    Correções

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.resource;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -7,11 +8,14 @@ import javax.ws.rs.core.MediaType;
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
 
+@Path("carrinhos")
 public class CarrinhoResource {
-	@Path("carrinhos")
-	@Produces(MediaType.APPLICATION_XML)
-	public String busca() {
-		Carrinho carrinho = new CarrinhoDAO().busca(1l);
-		return carrinho.toXML();
-	}
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String busca() {
+        Carrinho carrinho = new CarrinhoDAO().busca(1l);
+        return carrinho.toXML();
+    }
+
 }
